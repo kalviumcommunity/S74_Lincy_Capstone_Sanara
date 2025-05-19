ec43145fa1a2e57566f82eaa12a36488c2d350cd
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2025-05-19 23:07:52 +0530

    Used JWT in token creation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,8 @@
-// routes/authRoutes.js
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
   try {
@@ -14,6 +14,18 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     res.status(400).json({ error: 'User creation failed' });
   }
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ error: 'Invalid credentials' });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ error: 'Wrong password' });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.json({ token });
 });
 
 module.exports = router;
