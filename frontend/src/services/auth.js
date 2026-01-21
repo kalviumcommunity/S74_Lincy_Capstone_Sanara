ac1377778545ca9775285788b22dd35554f9cfe4
lincyrajasekaran3 <hp@DESKTOP-6433J6U>
2026-01-21 10:44:23 +0530

    google auth debugging

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,16 +1,24 @@
 import api from "./api";
 
-export const login = async ({ email, password }) => {
-  const res = await api.post("/auth/login", { email, password });
-  localStorage.setItem("token", res.data.token);
-  return res.data;
-};
-
+/* EMAIL SIGNUP */
 export const signup = async ({ email, password }) => {
-  const res = await api.post("/auth/register", { email, password });
+  const res = await api.post("/auth/register", {
+    email,
+    password,
+  });
   return res.data;
 };
 
-export const logout = () => {
-  localStorage.removeItem("token");
+/* EMAIL LOGIN */
+export const login = async ({ email, password }) => {
+  const res = await api.post("/auth/login", {
+    email,
+    password,
+  });
+
+  if (res.data.token) {
+    localStorage.setItem("token", res.data.token);
+  }
+
+  return res.data;
 };
